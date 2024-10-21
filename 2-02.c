0d2f196d9ce3c27a42accadb2ece29aadc0c1419
katepavlu <realkpavel@gmail.com>
2024-10-21 11:08:44 +0200

    Update 2-02.c

--- 2-02.c
+++ 2-02.c
@@ -4,11 +4,13 @@
  * without using && || */
 int main()
 {
-    for(i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i) {
+    int lim  = 0xdeadbeef;
+    char c = 0;
+    for(int i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i) {
         /* code goes here */
     }
     
-    for(i=0; i<lim-1; ++i) {
+    for(int i=0; i<lim-1; ++i) {
         if( (c=getchar()) == '\n')
             break;
         else if( c == EOF )
