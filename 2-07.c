764c75f7b230717feacdcde72ca4ed308aae6263
katepavlu <realkpavel@gmail.com>
2024-10-21 11:08:44 +0200

    Create 2-07.c

---
+++ 2-07.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* return x with n bits beginning at position p inverted */
+unsigned int invert(unsigned int x, unsigned int p, unsigned int n) {
+    unsigned int mask = 0;
+    
+    for(int i = 0; i<n; i++) {
+        mask += 1L<<i;
+    }
+    mask = mask << (sizeof(x)*8 - p - n); 
+    
+    unsigned int tmp = x & mask;
+    
+    x = x & ~mask; //first zero out the region
+    return x | (~tmp & mask);
+}
+
+int main()
+{
+    unsigned int x = 2;
+    printf("Hello World%u\n", invert(x, 29, 3));
+
+    return 0;
+}
