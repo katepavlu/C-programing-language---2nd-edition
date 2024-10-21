0c111ff667fe38ff892a13312e9be6562053295d
katepavlu <realkpavel@gmail.com>
2024-10-21 11:08:44 +0200

    Update 4-13.c

--- 4-13.c
+++ 4-13.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
 
+void reverse(char *str, int depth, const int len) {
+    char tmp = str[depth];
+    str[depth] = str[len - 1 - depth];
+    str[len - 1 - depth] = tmp;
+    if(depth < len - 1 - depth)
+        reverse(str, ++depth, len);
+}
+
+int main() {
+    char str[] = "The ultimate showdown of ultimate destiny\n";
+
+    reverse(str, 0, strlen(str));
+    printf("%s", str);
+
+    return 0;
+}
